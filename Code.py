ee0251794ac7d01cc8533aa267e1584933f779f7
Kate Lee <kateleext@gmail.com>
2021-05-12 18:13:53 +0800

    3.0 New function added: retrive records from a specific date.

--- Code.py
+++ Code.py
@@ -31,14 +31,14 @@ class Records:
             self.tag = "dinner"
         else:
             self.tag = "night"
+        
+        #Print
         print(timestamp.date())
         print("Record for {} logged.".format(self.tag))
         print("mmol/L: {}".format(self.mmol_level))
         print(self.evaluation)
 
-print("This is Yen's Blood Sugar Log.")
-print("Please enter your current reading.")
-
+#Functionalities
 def write_record(record):
     record_date = record.timestamp.date()
     record_time = record.timestamp.time()
@@ -47,8 +47,37 @@ def write_record(record):
         writer.writerow([record_date, record_time, record.tag, record.mmol_level, record.mgdl_level])
         log.close()
 
-mgdl_level = float(input())
-new_record = Records(mgdl_level)
-write_record(new_record)
+def retrieve_record(date):
+    retrieved = []
+    with open('/Users/kate/Desktop/Hustle/coding/CS101_Project/project/log.csv', 'r') as log:
+        reader = csv.reader(log, delimiter = ",")
+        for line in reader:
+            if line[0] == date:
+                retrieved.append([line[2], line[3]])
+        for record in retrieved:
+            print(record[0] + "\t" + record[1])
+    
+
+
+#Screen
+print("This is Yen's Blood Sugar Log. Please select your action.")
+print("1. New Record    2. Retrieve Record")
+
+#Input
+action = input()
+if action == 1:
+    mgdl = float(input("Current Reading:"))
+    new_record = Records(mgdl)
+    write_record(new_record)
+elif action == 2:
+    x = str(input("Date in YYYYMMDD format:"))
+    convert_date = datetime.strptime(x, '%Y%m%d')
+    read_date = convert_date.strftime('%Y-%m-%d')
+    retrieve_record(read_date)
+
+
+
+
+
 
 
